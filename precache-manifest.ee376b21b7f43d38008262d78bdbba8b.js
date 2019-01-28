26f6c145d928daf04b7a558a82a71c5bc3d8c6c0
edgar rodriguez <edgarj.rodriguezg@gmail.com>
2019-01-28 11:38:21 -0300

    Updates

---
+++ precache-manifest.ee376b21b7f43d38008262d78bdbba8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/board-price/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "036fc34dea8b5cbb554a",
+    "url": "/board-price/static/js/runtime~main.036fc34d.js"
+  },
+  {
+    "revision": "2bdcdbb6d24671cf1279",
+    "url": "/board-price/static/js/main.2bdcdbb6.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/board-price/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "2bdcdbb6d24671cf1279",
+    "url": "/board-price/static/css/main.6f49f4ac.chunk.css"
+  },
+  {
+    "revision": "3e768ee0f24230613a58a5934dec771a",
+    "url": "/board-price/index.html"
+  }
+];
